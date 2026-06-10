74bafb5913acb64abf58db1e0d352c1fcc0124a6
Tharindu Dilshan <dilshantharindu261@gmail.com>
2026-06-10 23:09:12 +0530

    implemented searchStudent function for search student by student ID

--- student-management-system/main.cpp
+++ student-management-system/main.cpp
@@ -85,10 +85,50 @@ public:
             cout << "=======================================" << endl;
         }
     }
+
+    void searchStudent(int studentId)
+    {
+        if (head == nullptr)
+        {
+            cout << "\nThe student list is empty.\n"
+                 << endl;
+            return;
+        }
+
+        Student *current = head;
+        while (current != nullptr)
+        {
+            if (current->id == studentId)
+            {
+                cout << "\nStudent found:\n"
+                     << endl;
+                cout << "ID   : " << current->id << endl;
+                cout << "Name : " << current->name << endl;
+                cout << "GPA  : " << current->GPA << endl;
+
+                return;
+            }
+
+            current = current->next;
+        }
+
+        cout << "\nStudent with ID " << studentId << " not found.\n"
+             << endl;
+    }
 };
 
 int main()
 {
-    
+    StudentList st;
+    st.addStudent(1, "Alice", 3.5);
+    st.addStudent(2, "Bob", 3.8);
+    st.addStudent(3, "Charlie", 3.2);
+    st.addStudent(4, "David", 3.9);
+
+    st.viewStudents();
+
+    st.searchStudent(2);
+    st.searchStudent(1);
+    st.searchStudent(5);
     return 0;
 }
